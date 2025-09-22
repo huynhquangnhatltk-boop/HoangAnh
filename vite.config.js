2d30831b1930b4c0308a4268445790b8f81456dd
Quang Nhật <quangnhat@MacBook-Pro-Quang-Nhat.local>
2025-09-22 14:39:50 +0700

    Cập nhật

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/HoangAnh/'  // 👈 tên repo GitHub của bạn
+export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    // Chạy LAN
+    return {
+      plugins: [vue()],
+      server: {
+        host: "0.0.0.0",
+        port: 5173
+      }
+    }
+  } else {
+    // Build deploy GitHub Pages
+    return {
+      plugins: [vue()],
+      base: '/HoangAnh/'   // 👈 repo name của bạn
+    }
+  }
 })
